3485610b7200f024cd567c7e955b2a184d58c90c
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-12 02:49:16 -0300

    react-router-dom initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import "./assets/CSS/App.css";
+
+import { Routes, Route } from "react-router-dom";
+
+import SignIn from "./pages/SignIn";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import "./assets/CSS/_reset.css";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
