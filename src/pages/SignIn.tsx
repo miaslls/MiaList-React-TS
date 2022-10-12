31778fd2a3d87ad9516ec7866c744e62875d1722
miaslls <97785569+miaslls@users.noreply.github.com>
2022-10-12 02:49:49 -0300

    SignIn layout

---
+++ src/pages/SignIn.tsx
@@ -0,0 +1,26 @@
+import "../assets/CSS/SignIn.css";
+
+function SignIn() {
+  return (
+    <>
+      <div className="login-outer-container">
+        <main className="login-inner-container">
+          <h1>
+            Mia<span>List</span>
+          </h1>
+
+          <form className="login-form">
+            <input type="text" name="username" placeholder="username" />
+            <input type="password" name="password" placeholder="password" />
+
+            <div className="login-button clickable">
+              <i className="bi bi-check-circle"></i>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default SignIn;
